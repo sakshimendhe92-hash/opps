7d1bb861b87f8071c6c03db9f30d764cf2094c8e
sakshimendhe92-hash <sakshimendhe92@gmail.com>
2026-02-04 21:08:43 +0530

    Add files via upload

---
+++ MyProgramCube.java
@@ -0,0 +1,26 @@
+
+class MyProgramCube {
+    public static void main(String[] args) {
+
+        float area;
+        Cube c1 = new Cube();
+
+        area = c1.calArea(10, 10, 10);
+        System.out.println("area = " + area);
+    }
+}
+
+class Cube {
+    float height;
+    float width;
+    float depth;
+
+    float calArea(float h, float w, float d) {
+        height = h;
+        width = w;
+        depth = d;
+
+        return height * width * depth;
+    }
+}
+
